d3be68e015706618046b0aa5208bb9a31053f839
mark <alufmark@gmail.com>
2025-04-23 08:54:15 +0300

    Add more style to header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -4,9 +4,19 @@ import { NavLink } from 'react-router-dom'
 export function AppHeader(){
     return(
         <section className='app-header flex'>
+            <div className='logo'>
+              <span className='blue'>T </span>  
+                 o y - M a r k e t
+            </div>
+            <div className='links-header'>
             <NavLink to="/">{('Home')}</NavLink>
             <NavLink to="/about">{('AboutUs')}</NavLink>
             <NavLink to="/toy">{('Toys')}</NavLink>
+            <NavLink to="/dashboard">{('Dashboard')}</NavLink>
+            </div>
+            <div >
+                <button className='user-btn'>Sign in</button>
+            </div>
         </section>
     )
 }
